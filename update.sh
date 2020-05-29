9b02dc2ad1d9fa5039b6bb944da8d7bcfe1f3581
22Kon <2782596816@qq.com>
2020-05-29 12:41:52 +0800

    update blog

--- update.sh
+++ update.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 hugo -D
 git add .
-echo "input commit info:"
-read -t 5 commit_info
-commit_info=${commit_info:-"update blog"}
-git commit -m "$commit_info"
+git commit -m "update blog"
 git push origin master
